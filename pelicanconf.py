92ef947a172ef3b5aeb968c95abd6a46135310d2
Herbert Yang <herbert.yang@gmail.com>
2017-03-01 11:20:39 -0800

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+# Basic settings
+AUTHOR = u'Herbert Yang'
+SITENAME = u'the Good the Bad and the Curious'
+SITEURL = 'https://guizishanren.com'
+DISQUS_SITENAME = 'goodbadcurious'
+GOOGLE_ANALYTICS = 'UA-714838-2'
+AUTHOR_EMAIL = 'guizishanren@qq.com'
+USE_FOLDER_AS_CATEGORY = True
+DEFAULT_CATEGORY = 'misc'
+DISPLAY_PAGES_ON_MENU = True
+SUMMARY_MAX_LENGTH = 100
+ADDTHIS_PROFILE = 'ra-538a1ad51a39747d'
+
+# Settings from pure-single theme
+
+COVER_IMG_URL = 'https://s3-us-west-2.amazonaws.com/guizishanren/img/shinjuku.jpg'
+
+PROFILE_IMG_URL = 'https://s.gravatar.com/avatar/106add5208f1a57290ed1164a903de21?s=80'
+
+FAVICON_URL = SITEURL + '/images/favicon.ico'
+
+MENUITEMS = (
+    ('Categories', '/categories.html'),
+    ('Tags', '/tags.html'),
+  )
+
+# Blogroll
+#LINKS = (('About Me', 'pages/about-me.html'))
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+GITHUB_URL = 'https://github.com/zire'
+TWITTER_USERNAME = 'herbertyang'
+
+# Feed generation is usually not desired when developing
+FEED_DOMAIN = 'https://feeds.feedburner.com'
+FEED_RSS = 'feeds/rss'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+# FEED_ATOM = 'GoodBadCurious/main.xml'
+# FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_URL = 'https://feeds.feedburner.com/GoodBadCurious'
+TAG_FEED_RSS = None
+# CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_RSS = 'feeds/category/.%s.rss'
+TRANSLATION_FEED_ATOM = None
+
+# Social widget
+SOCIAL = (
+          ('twitter', 'https://twitter.com/herbertyang'),
+          ('linkedin', 'https://www.linkedin.com/in/herbertyang'),
+          ('instagram', 'https://instagram.com/zire'),
+          ('github', 'https://github.com/zire'),
+          ('weibo', 'https://weibo.com/zire'),
+ #         ('flickr','https://www.flickr.com/photos/gankyun/'),
+ #         ('tumblr','https://zynphendale.tumblr.com'),
+ #         ('pinterest','https://www.pinterest.com/zire'),
+          ('quora', 'https://www.quora.com/Herbert-Yang'),
+          ('rss','https://www.guizishanren.com/feeds/rss'))
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+# Formatting date
+DEFAULT_DATE_FORMAT = ('%Y %m %d %A')
+
+# Formatting for urls
+DEFAULT_PAGINATION = 10
+
+#PAGE_URL = ('pages/{slug}.html')
+#PAGE_SAVE_AS = ('pages/{slug}.html')
+
+# ARTICLE_URL = "posts/{date:%Y}/{date:%m}/{slug}/"
+ARTICLE_URL = "{slug}"
+# ARTICLE_SAVE_AS = "posts/{date:%Y}/{date:%m}/{slug}/index.html"
+ARTICLE_SAVE_AS = "{slug}/index.html"
+
+CATEGORY_URL = "category/{slug}"
+CATEGORY_SAVE_AS = "category/{slug}/index.html"
+
+TAG_URL = "tag/{slug}"
+TAG_SAVE_AS = "tag/{slug}/index.html"
+
+PAGINATED_DIRECT_TEMPLATES = [('index'),('tags'), ('categories'),('archives')]
+DIRECT_TEMPLATES = ('index', 'tags', 'categories', 'archives')
+
+#PAGINATION_PATTERNS = (
+#    (1, '{base_name}/', '{base_name}/index.html'),
+#    (2, '{base_name}/', '{base_name}/index{number}.html'),
+#)
+
+# Generate yearly archive
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+
+# Show most recent posts first
+NEWEST_FIRST_ARCHIVES = False
+
+# Specify theme & plug-ins
+# THEME = "themes/bootstrap-HY"
+THEME = "themes/pure-single"
+PLUGINS = ['sitemap','neighbors','gravatar']
+
+# Specify important paths
+OUTPUT_PATH = 'output'
+PATH = 'content'
+STATIC_PATHS = ['images', 'GoodBadCurious',]
+PLUGIN_PATH = 'plugins'
+
+# Define sitemap parameters
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'daily',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
